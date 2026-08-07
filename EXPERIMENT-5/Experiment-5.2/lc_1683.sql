7a8169f98da1ec3de8284cb01b45a2be730b481c
disha <yashugoel786@gmail.com>
2026-08-07 10:25:58 +0530

    Add Experiment 5.2

---
+++ EXPERIMENT-5/Experiment-5.2/lc_1683.sql
@@ -0,0 +1,8 @@
+--Experiment-5.2=>
+--https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
+
+--Write your MySQL query statement below
+
+SELECT tweet_id 
+FROM Tweets
+WHERE LENGTH(content) > 15;
