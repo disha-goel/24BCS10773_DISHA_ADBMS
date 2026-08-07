29aa29e34c3cc509d9da77aa623579af42d2b274
disha <yashugoel786@gmail.com>
2026-08-07 15:30:20 +0530

    Add Homework  session-6

---
+++ HOMEWORK/Session-7/code1.sql
@@ -0,0 +1,9 @@
+--follow-up question
+--session-7=>code 1
+--https://www.codechef.com/learn/course/sql-intermediate/SQ00BS07/problems/GSQ78?tab=statement
+
+/* Write a query to output the details of employees in the table 'employee'. Limit to 5 rows. */
+
+SELECT *
+FROM employee
+LIMIT 5;
